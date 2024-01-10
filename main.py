cf7098166055ea0b660c18e6df2273babae80239
juheesvt <wnglk62@gmail.com>
2024-01-10 19:50:15 +0900

    fix: :bug: local time zone을 utc로 변경해서 시간대로 인해 발생하는 버그 수정

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import pytz
 
 def get_commits(user, repo, since, until):
     url=f"https://api.github.com/repos/{user}/{repo}/commits"
@@ -19,8 +20,26 @@ if __name__ == "__main__":
 
     user = "juheesvt"
     repo = "commit-check"
-    since = datetime.datetime.now().date().isoformat() 
-    until = (datetime.datetime.now() + datetime.timedelta(days=1)).date().isoformat()
+
+    # UTC 시간대로 날짜와 시간 설정
+    utc_zone = pytz.utc
+    local_zone = pytz.timezone('Asia/Seoul')  # 예시로 서울 시간대 사용
+
+    # 현재 시간을 서울 시간대로 설정하고 UTC로 변환
+    local_time = datetime.datetime.now(local_zone)
+    utc_time = local_time.astimezone(utc_zone)
+
+    # 'since'를 어제의 UTC 시간으로 설정
+    since = (utc_time - datetime.timedelta(days=1)).date().isoformat()
+    # 'until'을 오늘의 UTC 시간으로 설정
+    until = utc_time.date().isoformat()
+
 
     commits = get_commits(user, repo, since, until)
-    print(commits)
+    if commits:
+        print(f"총 커밋 수: {len(commits)}")
+        for commit in commits:
+            print(f"- 커밋 메시지: {commit['commit']['message']}")
+    else:
+        print("커밋 정보가 없습니다")
+
